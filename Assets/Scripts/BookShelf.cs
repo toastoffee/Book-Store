66f30c0792d147506d44aee870c8804566a387c5
toastoffee <931391442@qq.com>
2026-01-08 21:23:57 +0800

    add Book shelf

---
+++ Assets/Scripts/BookShelf.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookShelf : MonoBehaviour
+{
+    public int maxContent = 32;
+    
+    public List<BookVisual> allBookVisuals = new List<BookVisual>();
+    public List<Transform> anchorPoses = new List<Transform>();
+    
+    public List<BookData> allBooks = new List<BookData>();
+
+
+    private void Start()
+    {
+        for (int i = 0; i < maxContent; i++)
+        {
+            Transform anchor = new GameObject("anchor").transform;
+            anchor.SetParent(transform);
+            anchor.localPosition = allBookVisuals[i].transform.localPosition;
+            anchor.localRotation = allBookVisuals[i].transform.localRotation;
+            
+            allBookVisuals[i].transform.SetParent(anchor);
+            allBookVisuals[i].transform.localPosition = Vector3.zero;
+            anchorPoses.Add(anchor);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void UpdateVisuals()
+    {
+        
+    }
+}
